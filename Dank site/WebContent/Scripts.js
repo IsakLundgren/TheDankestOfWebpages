073f82dc4755008ca4b3605e505c7c826f2666f9
BuildTools <unconfigured@null.spigotmc.org>
2017-09-05 22:18:16 +0200

    Added correct linkage to the menu, additional js, sidebar adjust

--- Dank site/WebContent/Scripts.js
+++ Dank site/WebContent/Scripts.js
@@ -1,3 +1,12 @@
 function funct1() {
 	document.getElementById("A").src="https://media.tenor.com/images/874b4df9aba13edeb1839fab29c44a92/tenor.gif";
 }
+function funct2() {
+	var x, y, z, w;
+	x = 1268;
+	y = 1321;
+	z = 1422;
+	w = x + y + z + (20 * 3) + (26 * 3 * 2);
+	document.getElementById('Side').style.height='2000px'; // fråga om detta
+	console.log( 9 + 10 );
+}
